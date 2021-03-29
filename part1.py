9fb7795b868e52854f6d08c7742d0d3e3e70f846
AbsentAbsinthe <hemminglr@cardiff.ac.uk>
2021-03-29 14:31:50 +0100

    First commit

---
+++ part1.py
@@ -0,0 +1,18 @@
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+%matplotlib inline
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Ignore useless warnings (see SciPy issue #5998)
+import warnings
+warnings.filterwarnings(action="ignore", message="^internal gelsd")
