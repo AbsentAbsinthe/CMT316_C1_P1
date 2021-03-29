fb1a2b61ea174d64b05c0fe736b42516477c1f69
AbsentAbsinthe <hemminglr@cardiff.ac.uk>
2021-03-29 16:09:17 +0100

    Part1.py Classification First attempt
    Fails as it predicts all training data to be cheap

--- part1.py
+++ part1.py
@@ -1,18 +1,59 @@
-# Common imports
 import numpy as np
-import os
+import nltk
+import sklearn
+import operator
+import requests
 
-# to make this notebook's output stable across runs
-np.random.seed(42)
+train_path='D:/UNI/CMT316 - Applications of Machine Learning; Natural Language Processing and Computer Vision/Coursework 1/datasets_coursework1/real-state/train_full_Real-estate.csv'
 
-# To plot pretty figures
-%matplotlib inline
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-mpl.rc('axes', labelsize=14)
-mpl.rc('xtick', labelsize=12)
-mpl.rc('ytick', labelsize=12)
+dataset_file=open(train_path).readlines()
 
-# Ignore useless warnings (see SciPy issue #5998)
-import warnings
-warnings.filterwarnings(action="ignore", message="^internal gelsd")
+X_train=[]
+Y_train=[]
+for home in dataset_file[1:]:
+    home_linesplit=home.split(",")
+    vector_home_features=np.zeros(len(home_linesplit)-1)
+    for i in range(len(home_linesplit)-1):
+        vector_home_features[i]=float(home_linesplit[i])
+    X_train.append(vector_home_features)
+    if float(home_linesplit[-1]) < 30:
+        Y_train.append(0)
+    else:
+        Y_train.append(1)
+
+X_train_expensive=np.asarray(X_train)
+Y_train_expensive=np.asarray(Y_train) # This step is really not necessary, but it is recommended to work with numpy arrays instead of Python lists.
+
+print(Y_train_expensive)
+
+svm_clf_expensive=sklearn.svm.SVC(kernel="linear",gamma='auto') # Initialize the SVM model
+svm_clf_expensive.fit(X_train_expensive,Y_train_expensive) # Train the SVM model
+
+test_path='D:/UNI/CMT316 - Applications of Machine Learning; Natural Language Processing and Computer Vision/Coursework 1/datasets_coursework1/real-state/test_full_Real-estate.csv'
+
+test_dataset_file=open(train_path).readlines()
+
+true_expensive = 0
+false_expensive = 0
+true_cheap = 0
+false_cheap = 0
+
+for home in test_dataset_file[1:]:
+    test_home_linesplit=home.split(",")
+    vector_test_home_features=np.zeros(len(home_linesplit)-1)
+    for i in range(len(home_linesplit)-1):
+        vector_test_home_features[i]=float(home_linesplit[i])
+    prediction = svm_clf_expensive.predict([vector_test_home_features])
+    print(prediction)
+    if float(test_home_linesplit[-1]) < 30 and prediction == 0:
+        true_cheap += 1
+    elif float(test_home_linesplit[-1]) < 30 and prediction == 1:
+        false_expensive += 1
+    elif prediction == 1:
+        true_expensive += 1
+    else:
+        false_cheap += 1
+
+test_outcome=[true_expensive, false_expensive, true_cheap, false_cheap]
+
+print(test_outcome)
