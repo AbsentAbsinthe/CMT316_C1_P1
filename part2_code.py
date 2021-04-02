5e1904387f01d33cf69ddc2ff52d7fbeed79a2a1
AbsentAbsinthe <hemminglr@cardiff.ac.uk>
2021-04-02 12:16:04 +0100

    Part 2 Data Preprocessing

--- part2_code.py
+++ part2_code.py
@@ -9,16 +9,82 @@ import random
 from random import shuffle
 import requests
 
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
 classifications = ['business', 'entertainment', 'politics', 'sport', 'tech']
-training_data = []
+data = []
 test_data = []
 
 for cl in classifications:
     path = 'D:/UNI/CMT316 - Applications of Machine Learning; Natural Language Processing and Computer Vision/Coursework 1/datasets_coursework1/bbc/' + cl
     for file in os.listdir(path):
-        training_data.append([(open((path + '/' + str(file))).read()),cl])
+        data.append([(open((path + '/' + str(file))).read()),cl])
 
-shuffle(training_data)
+shuffle(data)
 
-for i in range(round(len(training_data) * 0.2)):
-    test_data.append(training_data.pop(random.randint(0,len(training_data))))
+
+for i in range(round(len(data) * 0.2)):
+    test_data.append(data.pop(random.randint(0,(len(data)-1))))
+
+
+lemmatizer = nltk.stem.WordNetLemmatizer()
+
+def get_list_tokens(string):
+    sentence_split=nltk.tokenize.sent_tokenize(string)
+    list_tokens=[]
+    for sentence in sentence_split:
+        list_tokens_sentence=nltk.tokenize.word_tokenize(sentence)
+    for token in list_tokens_sentence:
+        list_tokens.append(lemmatizer.lemmatize(token).lower())
+    return list_tokens
+
+# Add all possible stopwords to ensure that they aaren't counted in the most frequent words
+stopwords=set(nltk.corpus.stopwords.words('english'))
+stopwords.add('\n')
+stopwords.add('.')
+stopwords.add(',')
+stopwords.add('--')
+stopwords.add('``')
+stopwords.add('"')
+stopwords.add('(')
+stopwords.add(')')
+stopwords.add('-')
+stopwords.add(')')
+stopwords.add("'")
+stopwords.add("''")
+stopwords.add(':')
+
+dict_word_frequency={}
+for article in data:
+    sentence_tokens=get_list_tokens(article[0])
+    for word in sentence_tokens:
+        if word in stopwords: continue
+        if word not in dict_word_frequency: dict_word_frequency[word]=1
+        else: dict_word_frequency[word]+=1
+
+sorted_list = sorted(dict_word_frequency.items(), key=operator.itemgetter(1), reverse=True)[:1000]
+i=0
+
+frequent_words=[]
+for word,frequency in sorted_list:
+    frequent_words.append(word)
+
+def get_vector_text(list_vocab,string):
+    vector_text=np.zeros(len(list_vocab))
+    list_tokens_string=get_list_tokens(string)
+    for i, word in enumerate(list_vocab):
+        if word in list_tokens_string:
+            vector_text[i]=list_tokens_string.count(word)
+    return vector_text
+
+X_train = []
+Y_train = []
+
+for article in data:
+    vector_pos_review=get_vector_text(frequent_words,article[0])
+    X_train.append(vector_pos_review)
+    Y_train.append(article[1])
+
+
