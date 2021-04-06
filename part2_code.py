0aa0475b1e91a8f95c3695c4e546b24b5064fac7
AbsentAbsinthe <hemminglr@cardiff.ac.uk>
2021-04-06 17:15:01 +0100

    part_2 Second Feature

--- part2_code.py
+++ part2_code.py
@@ -4,6 +4,8 @@ import sklearn
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
 import operator
 import os
 import random
@@ -14,6 +16,7 @@ nltk.download('punkt') #Used to download necessary libraries
 nltk.download('wordnet')
 nltk.download('stopwords')
 
+#-----------------------------------------Data Handling and Test/Train split---------------------------------------------------------------------------
 classifications = ['business', 'entertainment', 'politics', 'sport', 'tech'] 
 data = []
 test_data = []
@@ -29,7 +32,7 @@ shuffle(data)
 for i in range(round(len(data) * 0.2)):
     test_data.append(data.pop(random.randint(0,(len(data)-1))))
 
-
+#-----------------------------------------Data Preprocessing and Feature 1 dictionairy creation---------------------------------------------------------------
 lemmatizer = nltk.stem.WordNetLemmatizer()
 
 def get_list_tokens(string):
@@ -57,20 +60,11 @@ stopwords.add("'")
 stopwords.add("''")
 stopwords.add(':')
 
-dict_word_frequency={}
+X_train = []
+Y_train = []
+
 for article in data:
-    sentence_tokens=get_list_tokens(article[0])
-    for word in sentence_tokens:
-        if word in stopwords: continue
-        if word not in dict_word_frequency: dict_word_frequency[word]=1
-        else: dict_word_frequency[word]+=1
-
-sorted_list = sorted(dict_word_frequency.items(), key=operator.itemgetter(1), reverse=True)[:1000]
-i=0
-
-frequent_words=[]
-for word,frequency in sorted_list:
-    frequent_words.append(word)
+    Y_train.append(classifications.index(article[1]))
 
 def get_vector_text(list_vocab,string):
     vector_text=np.zeros(len(list_vocab))
@@ -79,29 +73,68 @@ def get_vector_text(list_vocab,string):
         if word in list_tokens_string:
             vector_text[i]=list_tokens_string.count(word)
     return vector_text
+    frequent_words.append(word)
 
-X_train = []
-Y_train = []
+#-----------------------------------------Feature 1- Sparse Vector of 750 most common words---------------------------------------------------------
+dict_word_frequency={}
+for article in data:
+    sentence_tokens=get_list_tokens(article[0])
+    for word in sentence_tokens:
+        if word in stopwords: continue
+        if word not in dict_word_frequency: dict_word_frequency[word]=1
+        else: dict_word_frequency[word]+=1
+
+sorted_list = sorted(dict_word_frequency.items(), key=operator.itemgetter(1), reverse=True)[:750]
+i=0
+
+frequent_words=[]
+for word,frequency in sorted_list:
+    frequent_words.append(word)
+
+#-----------------------------------------Feature 2- Weighted Vector----------------------------------------------------------------------------------
+articles = []
 
 for article in data:
-    article_vector=get_vector_text(frequent_words,article[0])
-    X_train.append(article_vector)
-    if article[1] == 'business':
-        Y_train.append(0)
-    elif article[1] == 'entertainment':
-        Y_train.append(1)
-    elif article[1] == 'politics':
-        Y_train.append(2)
-    elif article[1] == 'sport':
-        Y_train.append(3)
-    else:
-        Y_train.append(4)
+    articles.append(article[0])
 
+vectorizer = TfidfVectorizer(stop_words='english')
+X_train_2 = vectorizer.fit_transform(articles)
+
+feature_names = vectorizer.get_feature_names()
+
+ch2 = SelectKBest(chi2, k=750)
+X_train_2 = ch2.fit_transform(X_train_2, Y_train)
+
+feature_names = [feature_names[i] for i in ch2.get_support(indices=True)]
+
+same = 0
+diff = 0
+
+for name in feature_names:
+    if name in frequent_words:
+        same += 1
+    else:
+        diff += 1
+
+print('\nUsing weighted word frequency vs counted word frequency gave ' + str(same)+ ' of the same words and ' + str(diff) + ' different words\n')
+
+all_features = feature_names
+all_features.append(frequent_words) 
+
+for article in data:
+    article_vector=get_vector_text(all_features,article[0])
+    X_train.append(article_vector)
+
+#-----------------------------------------Feature 3 - Names---------------------------------------------------------------------------------------
+
+
+
+#-----------------------------------------SVM train and test/predict------------------------------------------------------------------------------
 X_train_np=np.asarray(X_train)
 Y_train_np=np.asarray(Y_train)
 
 clf = sklearn.svm.SVC(decision_function_shape='ovo')
-clf.fit(X_train_np, Y_train_np)
+clf.fit(X_train, Y_train)
 
 true_classification = 0
 false_classification = 0
@@ -110,7 +143,7 @@ Y_true = []
 Y_predict = []
 
 for article in test_data:
-    article_vector=get_vector_text(frequent_words,article[0])
+    article_vector=get_vector_text(all_features,article[0])
     prediction = clf.predict([article_vector])
     Y_true.append(classifications.index(article[1]))
     Y_predict.append(prediction[0])
@@ -121,3 +154,4 @@ for article in test_data:
 print('\n')
 print(classification_report(Y_true, Y_predict))
 print('\n  True results:    ' + str(true_classification) + '\n False results:    ' + str(false_classification))
+
